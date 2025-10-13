db37994ef942e6556d4651636e95c02c2319f2b5
albertescote <albertescote03@gmail.com>
2025-10-13 16:41:40 +0200

    fix(locales): reorder locales array to prioritize Canadian and Spanish

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
-export const locales = ["en", "ca", "es"] as const;
+export const locales = ["ca", "es", "en"] as const;
 export type Locale = typeof locales[number];
 
 export const defaultLocale: Locale = "en";
